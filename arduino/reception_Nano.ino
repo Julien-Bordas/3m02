d1d47d9b94aa52995ba58c396cf24df47003ee0a
Julien-Bordas <80283501+Julien-Bordas@users.noreply.github.com>
2023-02-16 14:39:26 +0100

    reception_Nano

--- arduino/reception_Nano.ino
+++ arduino/reception_Nano.ino
@@ -1,26 +1,31 @@
-struct donneesMesureesDef
-{float voltageA; int16_t intValueA; float voltageB; uint8_t byteA; float voltageC;}__attribute__((packed));;
-donneesMesureesDef donneesAtransmettre;
 
-bool nouvellesDonnes = false;
+struct donneesMesureesDef //On définit une structure de données contenant 5 membres : 
+{float voltageA; int16_t intValueA; float voltageB; uint8_t byteA; float voltageC;}
+__attribute__((packed));; //"attribute((packed))" permet de s'assurer que les données de la structure soient empaquetées sans décalage.
+
+donneesMesureesDef donneesAtransmettre; //On définit une instance de la structure "donneesMesureesDef".
+
+bool nouvellesDonnes = false; //Indique si de nouvelles données ont été reçues ou non.
 
 void setup()
 {
-      Serial.begin(9600);
-      //Serial2.begin(115200);
+      Serial.begin(19200); //On lance la communication série avec une vitesse de 19200 bauds.
+      //Serial1.begin(9600);
 }
 
 void loop()
 {
-      if (Serial.available()) //2
+      //if (Serial1.available())
+      if (Serial.available()) //Vérifie si des données sont disponibles dans le port série.
       {
-            Serial.readBytes((byte*)&donneesAtransmettre, sizeof donneesAtransmettre); //2
-            nouvellesDonnes = true;
+            //Serial1.readBytes((byte*)&donneesAtransmettre, sizeof donneesAtransmettre);
+            Serial.readBytes((byte*)&donneesAtransmettre, sizeof donneesAtransmettre); //Si les données sont disponibles, le programme les lit dans la strucutre  "donneesAtransmettre".
+            nouvellesDonnes = true; 
       }
       
-      if (nouvellesDonnes)
+      if (nouvellesDonnes) //Si "nouvellesDonnes" est vrai : 
       {
-            Serial.print(donneesAtransmettre.voltageA); Serial.print("\t");
+            Serial.print(donneesAtransmettre.voltageA); Serial.print("\t"); //Imprime les membres de la structure "donneesAtransmettre" et les sépare par des tabulations.
             Serial.print(donneesAtransmettre.voltageB); Serial.print("\t");
             Serial.print(donneesAtransmettre.voltageC); Serial.print("\t");
             Serial.print(donneesAtransmettre.intValueA); Serial.print("\t");
